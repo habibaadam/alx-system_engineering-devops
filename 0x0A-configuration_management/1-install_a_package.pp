a509d42fe947659d3b45aa275c560cdc7763ba61
habibaadam <habiepalmer@gmail.com>
2023-11-24 12:27:31 +0000

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-# install flask from pip3
+# Installing a especific version of flask (2.1.0)
 
 package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
