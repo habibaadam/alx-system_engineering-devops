5cd9976ef8e2c319894c948b1f43df869e5309cf
habibaadam <habiepalmer@gmail.com>
2023-11-25 09:23:59 +0000

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Installing a especific version of flask (2.1.0)
-package {'flask':
+# this pp file installs a package
+package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
