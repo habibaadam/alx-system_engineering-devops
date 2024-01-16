ecae85084d5f0c20b5e6cb99b516167227cc6b59
habibaadam <habiepalmer@gmail.com>
2024-01-16 09:37:34 -0800

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-A function that allows you to retrieve information
-about subscribers on a specified subreddit in Reddit.
-"""
+"""Function that queries the Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Retrieve and provide the overall count of
-    subscribers for a specified subreddit
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Returns the number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom-User-Agent'}
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+
+            return subscribers
+        else:
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
