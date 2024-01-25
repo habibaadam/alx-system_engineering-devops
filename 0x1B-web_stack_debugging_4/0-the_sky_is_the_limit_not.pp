fc7a3fa4464e8bd9459c5c3ab382d2e6631de86c
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-25 14:50:31 +0000

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# allow a maximum of 500 file descriptors and then restart the Nginx service
-exec { 'change-ulimit-for-nginx':
-  command => 'sed -i "/^ULIMIT/c ULIMIT=\"-n 500\"" /etc/default/nginx; service nginx restart',
-  path    => ['/usr/bin', '/bin']
+# fix number of file descriptors and then restart the Nginx service
+exec {'Change the ulimit value':
+        command => 'sed -i "s/15/1024/g" /etc/default/nginx; service nginx restart',
+        path    => '/usr/bin:/bin'
 }
