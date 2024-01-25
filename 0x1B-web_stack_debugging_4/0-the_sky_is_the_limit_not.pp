2f921128e25c46d673720a36b70eb48c62d2d477
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-25 17:41:56 +0000

    updated puppet script

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
-# Increases the ulimit of an nginx server
+# the ulimit of an nginx server increased
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Restart Nginx
+# Restarts Nginx
 -> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
